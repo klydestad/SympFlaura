6965e025ba437c4ee429851ac59775b2eabda1a8
Kiersten Lyde-Stad <kikilydestad@outlook.com>
2025-05-07 14:46:50 -0700

    init

---
+++ sympflaura/backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+from model import predict_flare
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    df = pd.DataFrame([data])
+    prediction = predict_flare(df)
+    return jsonify({'prediction': prediction})
+
+if __name__ == '__main__':
+    app.run(debug=True)
