10fa344c25ba4d01e6c7891b4404fc7bdc35095b
Kiersten Lyde-Stad <kikilydestad@outlook.com>
2025-05-07 17:01:26 -0700

    bug fixes for training model

--- backend/model.py
+++ backend/model.py
@@ -2,7 +2,7 @@ import pandas as pd
 import joblib
 
 # Load the trained model once
-model = joblib.load("backend/flare_model.pkl")
+model = joblib.load("flare_model.pkl")
 
 def predict_flare(df):
     features = df[["fatigue", "pain", "brain_fog"]]
